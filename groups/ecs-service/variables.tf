9c469f7f02fc752e4c9fb65bc25fc1100d4ed29a
Michael Acreman <macreman.ch@gmail.com>
2023-04-27 11:16:39 +0100

    update library version and default scaling and sizing values

--- groups/ecs-service/variables.tf
+++ groups/ecs-service/variables.tf
@@ -50,17 +50,17 @@ variable "docker_registry" {
 variable "desired_task_count" {
   type = number
   description = "The desired ECS task count for this service"
-  default = 1
+  default = 1 # defaulted low for dev environments, override for production
 }
 variable "required_cpus" {
   type = number
-  description = "The required cpu count for this service"
-  default = 1
+  description = "The required cpu resource for this service. 1024 here is 1 vCPU"
+  default = 128 # defaulted low for dev environments, override for production
 }
 variable "required_memory" {
   type = number
   description = "The required memory for this service"
-  default = 512
+  default = 128 # defaulted low for dev environments, override for production
 }
 
 # ------------------------------------------------------------------------------
